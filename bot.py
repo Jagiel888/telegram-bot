1d090c96996e56fe7625a2df421bd46a1ef372fd
Jagiel888 <156862411+Jagiel888@users.noreply.github.com>
2026-03-01 13:37:30 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Hej 👋 Bot działa!")
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, "Napisałeś: " + message.text)
+
+print("Bot działa...")
+bot.infinity_polling()
